ffa6731c849db520e2c04b8ea36e51d40873287f
Ajinkya Saivar <172878214+ajinkya682@users.noreply.github.com>
2026-06-02 13:33:47 +0530

    Add user model and types for authentication

---
+++ src/models/user.model.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters"],
+  },
+});
